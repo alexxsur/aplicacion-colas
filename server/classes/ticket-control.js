fcd6392a76630362ef162285f5c5e97f2769c098
Alex <alexxsur@gmail.com>
2020-04-02 20:35:08 -0500

    Coleccion de tickets pendientes por atender

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -1,15 +1,24 @@
 const fs = require('fs');
 
+class Ticket {
+    constructor(numero, escritorio) {
+        this.numero = numero;
+        this.escritorio = escritorio;
+    }
+}
+
 class TicketControl {
 
     constructor() {
         this.ultimo = 0;
         this.hoy = new Date().getDate();
+        this.tickets = [];
 
         let data = require('../data/data.json')
 
         if (data.hoy === this.hoy) {
             this.ultimo = data.ultimo;
+            this.tickets = data.tickets;
         } else {
             this.reiniciarConteo();
         }
@@ -17,6 +26,8 @@ class TicketControl {
 
     siguiente() {
         this.ultimo += 1;
+        let ticket = new Ticket(this.ultimo, null);
+        this.tickets.push(ticket);
         this.grabarArchivo();
         return `Ticket ${ this.ultimo }`;
     }
@@ -27,6 +38,8 @@ class TicketControl {
 
     reiniciarConteo() {
         this.ultimo = 0;
+        this.tickets = [];
+
         console.log('Se ha inicializado el sistema');
         this.grabarArchivo();
     }
@@ -34,7 +47,8 @@ class TicketControl {
     grabarArchivo() {
         let jsonData = {
             ultimo: this.ultimo,
-            hoy: this.hoy
+            hoy: this.hoy,
+            tickets: this.tickets
         }
 
         let jsonDataString = JSON.stringify(jsonData);
